b112fd7b2524f0580372e3ef5753266a8933bf73
Nochn0i <parlova08@proton.me>
2026-06-23 08:05:30 +0545

    added ip_sniffer<<completed>> and snake<<pending>>

---
+++ snake/src/draw.rs
@@ -0,0 +1,48 @@
+#![allow(unused)]
+
+use piston_window::graphics::Graphics;
+use piston_window::graphics::types::Color;
+use piston_window::graphics::{Context, rectangle};
+
+const BLOCK_SIZE: f64 = 25.0;
+
+pub fn to_coord(game_coord: i32) -> f64 {
+    (game_coord as f64) * BLOCK_SIZE
+}
+
+pub fn draw_block<G: Graphics>(color: Color, x: i32, y: i32, con: &Context, g: &mut G) {
+    let gui_x = to_coord(x);
+    let gui_y = to_coord(y);
+
+    rectangle(
+        color,
+        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+        con.transform,
+        g,
+    );
+}
+
+pub fn draw_rectangle<G: Graphics>(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    con: &Context,
+    g: &mut G,
+) {
+    let x = to_coord(x);
+    let y = to_coord(y);
+
+    rectangle(
+        color,
+        [
+            x,
+            y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64),
+        ],
+        con.transform,
+        g,
+    );
+}
---
+++ snake/src/game.rs
@@ -0,0 +1,100 @@
+#![allow(unused)]
+
+use piston_window::graphics::types::Color;
+use piston_window::graphics::{Context, Graphics};
+use piston_window::*;
+
+use crate::draw::{draw_block, draw_rectangle};
+use crate::snake::{Direction, Snake};
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+    snake: Snake,
+
+    food_exist: bool,
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Game {
+        Game {
+            snake: Snake::new(2, 2),
+            waiting_time: 0.0,
+            food_exist: true,
+            food_x: 6,
+            food_y: 4,
+            width,
+            height,
+            game_over: false,
+        }
+    }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None,
+        };
+
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        self.update_snake();
+    }
+
+    pub fn draw<G: Graphics>(&self, con: &Context, g: G) {
+        self.snake.draw(con, g);
+
+        if self.food_exist {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        }
+
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g);
+
+        if self.game_over {
+            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        }
+    }
+
+    pub fn update(&mut self, delta_time: f64) {
+        self.waiting_time += delta_time;
+
+        if self.game_over {
+            if self.waiting_time > RESTART_TIME {
+                self.restart();
+            }
+            return;
+        }
+
+        if !self.food_exist {
+            self.add_food();
+        }
+
+        if self.waiting_time > MOVING_PERIOD {
+            self.update_snake(None);
+        }
+    }
+}
